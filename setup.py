d36d6eebbbd6a8a8b0e15f37b128ee18ba2d50dc
Sacha Duverger (on personal computer) <sacha.duverger@inrae.fr>
2021-03-06 22:57:45 +0100

    Complete setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,12 @@ setup(
     version='1.0.0',
     description='Python interface to generate CB-Geo mpm input files',
     author='Sacha Duverger',
+    url='git_repos@axp20009:~/pycbg.git',
+    install_requires=[
+        'numpy',
+        'gmsh',
+        'pandas',
+        'matplotlib'
+    ],
     packages=find_packages(include=['pycbg/*'])
 )

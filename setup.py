67ed47654dfa29a427dcae96351eab6a1b32e00d
Sacha Duverger (on personal computer) <sacha.duverger@inrae.fr>
2021-03-06 20:15:20 +0100

    Change folder organisation for pip install

--- setup.py
+++ setup.py
@@ -3,5 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pycbg',
     version='1.0.0',
-    packages=find_packages(include=['preprocessing.py', 'postprocessing.py'])
+    description='Python interface to generate CB-Geo mpm input files',
+    author='Sacha Duverger',
+    packages=find_packages(include=['pycbg/*'])
 )

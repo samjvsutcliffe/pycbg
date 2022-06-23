24597d26409f5f1d571b90e04fbc5e99e6ed0103
Sacha Duverger <sacha.duverger@inrae.fr>
2022-06-23 14:12:42 +0200

    Improve batch's bash script

--- src/pycbg/preprocessing.py
+++ src/pycbg/preprocessing.py
@@ -1187,7 +1187,9 @@ def setup_batch(script_path, params, directory='', cbgeo_executable=None, ncores
         batch_launcher_file.write("#!/bin/bash\n\n")
         batch_launcher_file.write("""n_sim_slot_available="{:d}"\necho "n_sim_slot_available=$n_sim_slot_available" > /tmp/n_sim_slot_available.sh\n\n""".format(n_simultaneous_sim))
 
-    batch_launcher_file.write("""echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\nStarting batch from directory {:}\n"\n\n""".format(directory))
+        batch_launcher_file.write("""echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\nStarting batch from directory {:}\n"\n\n""".format(directory))
+        batch_launcher_file.write("""startT=$(date +%s)\n\n""")
+
     for sim_id, param_set in enumerate(param_sets):
         sim_title = "sim{:d}".format(sim_id)
         sim_dir = directory + sim_title + "/"
@@ -1216,10 +1218,8 @@ def setup_batch(script_path, params, directory='', cbgeo_executable=None, ncores
             batch_launcher_file.write("""( echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tStarting {:}"; {:}{:} -f "$(pwd)/" -i {:}input_file.json >> {:}cbgeo.log; echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tFinished {:}"; . /tmp/n_sim_slot_available.sh; n_sim_slot_available=$((++n_sim_slot_available)); echo "n_sim_slot_available=$n_sim_slot_available" > /tmp/n_sim_slot_available.sh ) &\n""".format(sim_title, cbgeo_executable, cores_str, sim_dir, sim_dir, sim_title))
 
     table_file.close()
-    if set_executable: batch_launcher_file.close()
+    if set_executable: 
+        batch_launcher_file.write("""for job in `jobs -p`; do wait $job; done; echo -ne "\n\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tBatch over, total duration: $(date -u --date @$(( $(date +%s) - $startT )) +%H:%M:%S)\n" """)
+        batch_launcher_file.close()
     sys.exit()
-
-
-    
-
 

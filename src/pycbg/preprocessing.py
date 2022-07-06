6fc743d7a13c70f7694de097402af3378eca8ec2
Sacha Duverger <sacha.duverger@inrae.fr>
2022-07-06 15:26:33 +0200

    Improve batch's bash script

--- src/pycbg/preprocessing.py
+++ src/pycbg/preprocessing.py
@@ -1244,11 +1244,11 @@ def setup_batch(script_path, params, directory='', cbgeo_executable=None, ncores
         if set_executable: 
             batch_launcher_file.write("""until [ "$n_sim_slot_available" -gt "0" ] \ndo\n\tsleep 0.1\n\t. /tmp/n_sim_slot_available.sh\ndone\n""")
             batch_launcher_file.write("""n_sim_slot_available=$((--n_sim_slot_available)); echo "n_sim_slot_available=$n_sim_slot_available" > /tmp/n_sim_slot_available.sh \n""")
-            batch_launcher_file.write("""( startT{:}=$(date +%s); echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tStarting {:};\tlog file: {:}"; {:}{:} -f "$(pwd)/" -i {:}input_file.json >> {:}cbgeo.log; echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tFinished {:};\tduration: $(date -u --date @$(( $(date +%s) - $startT{:} )) +%H:%M:%S)"; . /tmp/n_sim_slot_available.sh; n_sim_slot_available=$((++n_sim_slot_available)); echo "n_sim_slot_available=$n_sim_slot_available" > /tmp/n_sim_slot_available.sh ) &\n""".format(sim_title, sim_title, sim_dir+"cbgeo.log", cbgeo_executable, cores_str, sim_dir, sim_dir, sim_title, sim_title))
+            batch_launcher_file.write("""( startT{:}=$(date +%s); echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tStarting {:};\tlog file: {:}"; {:}{:} -f "$(pwd)/" -i {:}input_file.json >> {:}cbgeo.log; echo -e "\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tFinished {:};\tduration: $(date -u --date @$(( $(date +%s) - $startT{:} )) +%j\ days,\ %H:%M:%S)"; . /tmp/n_sim_slot_available.sh; n_sim_slot_available=$((++n_sim_slot_available)); echo "n_sim_slot_available=$n_sim_slot_available" > /tmp/n_sim_slot_available.sh ) &\n""".format(sim_title, sim_title, sim_dir+"cbgeo.log", cbgeo_executable, cores_str, sim_dir, sim_dir, sim_title, sim_title))
 
     table_file.close()
     if set_executable: 
-        batch_launcher_file.write("""for job in `jobs -p`; do wait $job; done; echo -ne "\n\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tBatch over, total duration: $(date -u --date @$(( $(date +%s) - $startT )) +%H:%M:%S)\n" """)
+        batch_launcher_file.write("""for job in `jobs -p`; do wait $job; done; echo -ne "\n\e[90m$(date +'[%Y-%m-%d %T.%N]')\e[0m\tBatch over, total duration: $(date -u --date @$(( $(date +%s) - $startT )) +%j\ days,\ %H:%M:%S)\n" """)
         batch_launcher_file.close()
     sys.exit()
 
